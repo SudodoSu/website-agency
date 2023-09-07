d2509d06ca3c2aa252c74b34baa67f24a20ae3c3
Sudo <93739034+SudodoSu@users.noreply.github.com>
2023-09-07 23:44:36 +0200

    Navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,24 @@
 import "../App.css";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from "../assets/images/logo-min2.png";
 
 function Header() {
     return (
-        <>
-            <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
-              <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.2">
-                        Another action
-                      </NavDropdown.Item>
-                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item href="#action/3.4">
-                        Separated link
-                      </NavDropdown.Item>
-                    </NavDropdown>
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
-        </>
+        <header>
+            <nav className="navbar">
+              <div className="logo">
+                <a className="logo">
+                  <img src={Logo} alt="logo boost web"/>
+                </a>
+              </div>
+              
+              <ul className="navbar-lista">
+                <li><a>Početna</a></li>
+                <li><a>Zašto Mi</a></li>
+                <li><a>Reference</a></li>
+                <li><button type="button">Besplatne Konzultacije <span>zakaži odmah</span></button></li>
+              </ul>
+            </nav>
+        </header>
     );
 }
 

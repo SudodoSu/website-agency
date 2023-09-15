7c399739d28e556b825b94ac19c5f5bd0e645521
Sudo <93739034+SudodoSu@users.noreply.github.com>
2023-09-16 00:34:52 +0200

    pricing-section

--- src/components/Pricing/pricing-plan.js
+++ src/components/Pricing/pricing-plan.js
@@ -1,21 +1,23 @@
-function PricingPlan({articles}) {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+
+function PricingPlan({articles, background, backgroundOut, hover}) {
     return(
         <>
             {articles.map((article) => (
-                <article key={article.id}>
-                    <div>
+                <article key={article.num} className={`pricing-article ${(hover[article.id]) ? "growArticle" : ""}`} onMouseEnter={() => background(article.id)} onMouseLeave={() => backgroundOut(article.id)}>
+                    <div className="plan-title">
                         <h2>{article.title}</h2>
                     </div>
                     <div className="pricing-plan-price">
                         <h2>{article.price}</h2>
                     </div>
                     <ul className="pricing-description">
-                        <li>{article.des[0]}</li>
-                        <li>{article.des[1]}</li>
-                        <li>{article.des[2]}</li>
-                        <li>{article.des[3]}</li>
-                        <li>{article.des[4]}</li>
-                        <li>{article.des[5]}</li>
+                        <li>{(article.des[0].doesHave) ? <span><FontAwesomeIcon icon={faCheck} style={{color: "#112d4e",}} /></span> : <span><FontAwesomeIcon icon={faX} style={{color: "#112d4e",}} /></span>}{article.des[0].title}</li>
+                        <li>{(article.des[1].doesHave) ? <span><FontAwesomeIcon icon={faCheck} style={{color: "#112d4e",}} /></span> : <span><FontAwesomeIcon icon={faX} style={{color: "#112d4e",}} /></span>}{article.des[1].title}</li>
+                        <li>{(article.des[2].doesHave) ? <span><FontAwesomeIcon icon={faCheck} style={{color: "#112d4e",}} /></span> : <span><FontAwesomeIcon icon={faX} style={{color: "#112d4e",}} /></span>}{article.des[2].title}</li>
+                        <li>{(article.des[3].doesHave) ? <span><FontAwesomeIcon icon={faCheck} style={{color: "#112d4e",}} /></span> : <span><FontAwesomeIcon icon={faX} style={{color: "#112d4e",}} /></span>}{article.des[3].title}</li>
                     </ul>
                     <button className="pricing-button">Vidi Više</button>
                 </article>
--- src/components/pricing-section.js
+++ src/components/pricing-section.js
@@ -1,17 +1,51 @@
 import PricingPlan from "./Pricing/pricing-plan";
+import { useState } from "react";
 
 function PricingSection() {
-
+    
     const data = [
-        {id: 1, title: "Basic Plan", des: ["Graphic Design", "Web Design", "UI/UX", "HTML/CSS", "SEO Marketing", "Business Analysis"]},
-        {id: 2, title: "Standard Plan", des: ["Graphic Design", "Web Design", "UI/UX", "HTML/CSS", "SEO Marketing", "Business Analysis"]},
-        {id: 3, title: "Premium Plan", des: ["Graphic Design", "Web Design", "UI/UX", "HTML/CSS", "SEO Marketing", "Business Analysis"]}
+        {id: 1, title: "Basic Plan", price: "€ 100", des: [{title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: false}]},
+        {id: 2, title: "Standard Plan", price: "€ 200", des: [{title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: false}]},
+        {id: 3, title: "Premium Plan", price: "€ 300", des: [{title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: true}, {title: "Graphic Design", doesHave: true}]}
     ];
 
+
+    const initialHoverState = data.map((datas) => datas.num === 3);
+    const [hover, sethover] = useState(initialHoverState);
+
+
+    function background(e) {
+        sethover((event) => {
+            const handleHover = [...event];
+            handleHover[e] = true;
+            return handleHover;
+        });
+    }
+
+    function backgroundOut(e) {
+        sethover((event) => {
+            const handleHover = [...event];
+            handleHover[e] = false;
+            return handleHover;
+        });
+    }
+
+
     return(
-        <div className="pricing-section">
-            <PricingPlan articles={data}/>
-        </div>
+        <section className="pricing">
+            <div className="max-width">
+                <div className="pricing-title">
+                    <h1>Izaberite Svoj Plan</h1>
+                    <div className="section-two-line"></div>
+                    <p>
+                    Bacimo se na posao i krenimo dizajnirati tvoju Online prisutnost!
+                    </p>
+                </div>
+                <div className="pricing-section">
+                    <PricingPlan articles={data} background={background} backgroundOut={backgroundOut} hover={hover}/>
+                </div>
+            </div>    
+        </section>
     );
 }
 

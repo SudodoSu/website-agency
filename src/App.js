e783b34c3f6dfade56abd5c8877ff8440fea0feb
Sudo <93739034+SudodoSu@users.noreply.github.com>
2023-09-19 22:10:08 +0200

    Contact Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./components/pages/Home";
-import Contact from './components/pages/Contact';
+import ContactUs from './components/pages/Contact';
 import PrivacyPolicy from './components/pages/PrivacyPolicy';
 import Services from './components/pages/Services';
-import { useState } from "react";
 
 function App() {
-
-  const [hover, sethover] = useState(2);
-
-    function background(e) {
-        sethover(e);
-    }
-
-  const isHovered = (e) => e === hover;
 
   return (
     <div>
@@ -22,7 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />}/>
           <Route path='/home' element={<Home />}/>
-          <Route path='/contact' element={<Contact />}/>
+          <Route path='/contact' element={<ContactUs />}/>
           <Route path='/services' element={<Services />}/>
           <Route path='/privacy-policy' element={<PrivacyPolicy />}/>
         </Routes>
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,17 +1,56 @@
-import Footer from "../footer";
-import Header from "../header";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Header from '../header';
+import Footer from '../footer';
+import FristSection from '../first-section';
+import { useState } from 'react';
+import ContactImage from "../../assets/images/contact-img.png"
 
+export const ContactUs = () => {
+  const form = useRef();
 
-function Contact() {
-    return(
-        <>
-            <Header />
-            <main>
-                
-            </main>
-            <Footer />
-        </>
-    );
-}
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-export default Contact;
+    emailjs.sendForm('service_q0fv0h8', 'template_k2wqm7h', form.current, 'QslYBLVV8d39BKjwl')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <>
+        <Header />
+        <FristSection title="Contact Us" />
+
+        <main className='contact-us'>
+            <div className='contact-us-image'>
+                <img 
+                    src={ContactImage}
+                />
+            </div>
+            <div className='contact-us-outform'>
+                <form ref={form} onSubmit={sendEmail}>
+                        <div><input type="text" name="user_name" placeholder='Name'/></div>
+                        <div><input type="email" name="user_email" placeholder='Email'/></div>
+                        <div>
+                            <select id="selectBox" value='Choose Service'>
+                                <option value="Web Development">Option 1</option>
+                                <option value="Web Solutions">Option 2</option>
+                                <option value="Strategy and Research">Option 3</option>
+                            </select>
+                        </div>
+                        <div><textarea name="message" /></div>
+                        <div><input type="submit" placeholder='Send' /></div>
+                </form>
+            </div>
+            
+        </main>
+        <Footer />
+    </>
+  );
+};
+
+export default ContactUs;

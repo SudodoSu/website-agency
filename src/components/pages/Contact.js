c77a6731fad102d63f47db5359e8fbf0eff04d96
Sudo <93739034+SudodoSu@users.noreply.github.com>
2023-09-21 01:08:14 +0200

    Contact Upd

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import Header from '../header';
 import Footer from '../footer';
@@ -6,12 +6,21 @@ import FristSection from '../first-section';
 import { useState } from 'react';
 import ContactImage from "../../assets/images/contact-img.png"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons';
+
 export const ContactUs = () => {
   const form = useRef();
+  const [selectedFruit, setSelectedFruit] = useState('');
+  const [clicked, isClicked] = useState(false);
+  const [messageSent, setMessageSent] = useState(false);
+  const [closed, setClose] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
+    setMessageSent(true);
+    
     emailjs.sendForm('service_q0fv0h8', 'template_k2wqm7h', form.current, 'QslYBLVV8d39BKjwl')
       .then((result) => {
           console.log(result.text);
@@ -19,6 +28,20 @@ export const ContactUs = () => {
           console.log(error.text);
       });
   };
+
+  const handleSelectChange = (event) => {
+    setSelectedFruit(event.target.value);
+  };
+
+  const handleClick = () => { 
+    isClicked(true); 
+
+    setTimeout(() => {
+        isClicked(false);
+      }, 120);
+  }
+
+  const handleClose = () => setClose(true);
 
   return (
     <>
@@ -33,20 +56,30 @@ export const ContactUs = () => {
             </div>
             <div className='contact-us-outform'>
                 <form ref={form} onSubmit={sendEmail}>
-                        <div><input type="text" name="user_name" placeholder='Name'/></div>
-                        <div><input type="email" name="user_email" placeholder='Email'/></div>
+                        <div><input required type="text" name="from_name" placeholder='Name'/></div>
+                        <div><input required type="email" name="from_email" placeholder='Email'/></div>
                         <div>
-                            <select id="selectBox" value='Choose Service'>
-                                <option value="Web Development">Option 1</option>
-                                <option value="Web Solutions">Option 2</option>
-                                <option value="Strategy and Research">Option 3</option>
+                            <select required name="service" id="selectBox" value={selectedFruit} placeholder='Choose Service' onChange={handleSelectChange}>
+                                <option value="">Choose Service</option>
+                                <option value="Web Development">Web Development</option>
+                                <option value="Web Solutions">Web Solutions</option>
+                                <option value="Strategy and Research">Strategy and Research</option>
                             </select>
                         </div>
-                        <div><textarea name="message" /></div>
-                        <div><input type="submit" placeholder='Send' /></div>
+                        <div className='contact-us-textarea-div'><textarea name="message" placeholder='Message' /></div>
+                        <div className={`submit-button ${clicked ? "clicked" : ""}`}><button onClick={handleClick}><input type="submit" placeholder='Send' /></button></div>
                 </form>
             </div>
-            
+            {messageSent &&
+                <div className={`successfully-sent ${closed ? "closed" : ""}`}>
+                    <FontAwesomeIcon className='successfully-sent-check' icon={faCheck} size="2xl" style={{color: "#45F248",}} />
+                    <div className='successfully-sent-message'>
+                        <h2>Success</h2>
+                        <p>We have received your message.</p>
+                    </div>
+                    <FontAwesomeIcon onClick={handleClose} className="successfully-sent-close" icon={faX} style={{color: "#F9F7F7",}} />
+                </div>
+            }
         </main>
         <Footer />
     </>
